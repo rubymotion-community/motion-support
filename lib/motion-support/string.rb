6c4c5f0e47f791e919ca3284a904be1c31fd626e
Thomas Kadauke <tkadauke@imedo.de>
2013-04-09 15:30:01 -0400

    Ported String#constantize implementation from ActiveSupport

--- lib/motion-support/string.rb
+++ lib/motion-support/string.rb
@@ -61,11 +61,47 @@ class String
     singularize.camelize
   end
 
-  # This method looks up a class name in the ObjectSpace's constants
-  # allowing you to do something like this:
+  # Tries to find a constant with the name specified in the argument string.
   #
-  # "under_scored".classify.contantize.new
+  #   'Module'.constantize     # => Module
+  #   'Test::Unit'.constantize # => Test::Unit
+  #
+  # The name is assumed to be the one of a top-level constant, no matter
+  # whether it starts with "::" or not. No lexical context is taken into
+  # account:
+  #
+  #   C = 'outside'
+  #   module M
+  #     C = 'inside'
+  #     C               # => 'inside'
+  #     'C'.constantize # => 'outside', same as ::C
+  #   end
+  #
+  # NameError is raised when the name is not in CamelCase or the constant is
+  # unknown.
   def constantize
-    Object.const_get(self)
+    names = self.split('::')
+    names.shift if names.empty? || names.first.empty?
+
+    names.inject(Object) do |constant, name|
+      if constant == Object
+        constant.const_get(name)
+      else
+        candidate = constant.const_get(name)
+        next candidate if constant.const_defined?(name, false)
+        next candidate unless Object.const_defined?(name)
+
+        # Go down the ancestors to check it it's owned
+        # directly before we reach Object or the end of ancestors.
+        constant = constant.ancestors.inject do |const, ancestor|
+          break const    if ancestor == Object
+          break ancestor if ancestor.const_defined?(name, false)
+          const
+        end
+
+        # owner is in Object, so raise
+        constant.const_get(name, false)
+      end
+    end
   end
 end
--- spec/motion-support/string_spec.rb
+++ spec/motion-support/string_spec.rb
@@ -136,10 +136,89 @@ describe "string" do
   end
 
   describe "constantize" do
-    class SearchController; end
+    module Ace
+      module Base
+        class Case
+          class Dice
+          end
+        end
+        class Fase < Case
+        end
+      end
+      class Gas
+        include Base
+      end
+    end
 
-    it "makes a constant class name from a suitable string" do
-      "SearchControllers".classify.constantize.should == SearchController
+    class Object
+      module AddtlGlobalConstants
+        class Case
+          class Dice
+          end
+        end
+      end
+      include AddtlGlobalConstants
+    end
+    
+    it "should lookup nested constant" do
+      "Ace::Base::Case".constantize.should == Ace::Base::Case
+    end
+    
+    it "should lookup nested absolute constant" do
+      "::Ace::Base::Case".constantize.should == Ace::Base::Case
+    end
+    
+    it "should lookup nested inherited constant" do
+      "Ace::Base::Fase::Dice".constantize.should == Ace::Base::Fase::Dice
+    end
+    
+    it "should lookup nested included constant" do
+      "Ace::Gas::Case".constantize.should == Ace::Gas::Case
+    end
+    
+    it "should lookup nested constant included into Object" do
+      "Case::Dice".constantize.should == Case::Dice
+    end
+    
+    it "should lookup nested absolute constant included into Object" do
+      "Object::Case::Dice".constantize.should == Case::Dice
+    end
+    
+    it "should lookup constant" do
+      "String".constantize.should == String
+    end
+    
+    it "should lookup absolute constant" do
+      "::Ace".constantize.should == Ace
+    end
+    
+    it "should return Object for empty string" do
+      "".constantize.should == Object
+    end
+    
+    it "should return Object for double colon" do
+      "::".constantize.should == Object
+    end
+    
+    it "should raise NameError for unknown constant" do
+      lambda { "UnknownClass".constantize }.should.raise NameError
+    end
+    
+    it "should raise NameError for unknown nested constant" do
+      lambda { "UnknownClass::Ace".constantize }.should.raise NameError
+      lambda { "UnknownClass::Ace::Base".constantize }.should.raise NameError
+    end
+    
+    it "should raise NameError for invalid string" do
+      lambda { "An invalid string".constantize }.should.raise NameError
+      lambda { "InvalidClass\n".constantize }.should.raise NameError
+    end
+    
+    it "should raise NameError for nested unknown constant in known constant" do
+      lambda { "Ace::ConstantizeTestCases".constantize }.should.raise NameError
+      lambda { "Ace::Base::ConstantizeTestCases".constantize }.should.raise NameError
+      lambda { "Ace::Gas::Base".constantize }.should.raise NameError
+      lambda { "Ace::Gas::ConstantizeTestCases".constantize }.should.raise NameError
     end
   end
 end
