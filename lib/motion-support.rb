1dfc01d853c6868ccc6863607cc8b5604f094629
Thomas Kadauke <tkadauke@imedo.de>
2013-04-25 00:49:36 +0200

    Don't change application settings from within the gem. Instead print out a very visible warning when trying to use automatic dependency detection with motion-support. Thanks to @aceofspades for convincing me :-)

--- lib/motion-support.rb
+++ lib/motion-support.rb
@@ -2,6 +2,4 @@ require "motion-require"
 
 Motion::Require.all(Dir.glob(File.expand_path('../motion-support/**/*.rb', __FILE__)))
 
-Motion::Project::App.setup do |app|
-  app.detect_dependencies = false
-end
+at_exit { puts "\nWARNING: Automatic dependency detection does not work with motion-support. Turn it off in your Rakefile:\n\n  app.detect_dependencies = false\n\n" if Motion::Project::App.config.detect_dependencies }
