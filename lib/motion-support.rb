a0a640db2f39d3dbb19985efb53385b15d59314a
Thomas Kadauke <tkadauke@imedo.de>
2013-04-24 23:42:34 +0200

    Force turning off dependency detection since that makes the build system crash

--- lib/motion-support.rb
+++ lib/motion-support.rb
@@ -1,4 +1,7 @@
-require "motion-support/version"
 require "motion-require"
 
 Motion::Require.all(Dir.glob(File.expand_path('../motion-support/**/*.rb', __FILE__)))
+
+Motion::Project::App.setup do |app|
+  app.detect_dependencies = false
+end
