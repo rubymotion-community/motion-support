d142d454d15b9303e100e6a7a895c6f96f7e244b
Thomas Kadauke <tkadauke@imedo.de>
2013-04-24 22:25:11 +0200

    Add missing documentation

--- lib/motion-support/core_ext/array/access.rb
+++ lib/motion-support/core_ext/array/access.rb
@@ -1,15 +1,27 @@
 class Array
   # Returns the tail of the array from +position+.
+  #
+  #   %w( a b c d ).from(0)  # => ["a", "b", "c", "d"]
+  #   %w( a b c d ).from(2)  # => ["c", "d"]
+  #   %w( a b c d ).from(10) # => []
+  #   %w().from(0)           # => []
   def from(position)
     self[position, length] || []
   end
 
   # Returns the beginning of the array up to +position+.
+  #
+  #   %w( a b c d ).to(0)  # => ["a"]
+  #   %w( a b c d ).to(2)  # => ["a", "b", "c"]
+  #   %w( a b c d ).to(10) # => ["a", "b", "c", "d"]
+  #   %w().to(0)           # => []
   def to(position)
     first position + 1
   end
 
-  # Returns the second element in an array if it exists, otherwise nil.
+  # Equal to <tt>self[1]</tt>.
+  #
+  #   %w( a b c d e ).second # => "b"
   def second
     self[1]
   end
