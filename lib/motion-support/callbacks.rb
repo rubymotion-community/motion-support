1731b9f803979e3d5b3e879c6db9981a3d05c039
Thomas Kadauke <thomas.kadauke@googlemail.com>
2013-05-14 13:44:34 +0200

    Added missing dependencies

--- lib/motion-support/callbacks.rb
+++ lib/motion-support/callbacks.rb
@@ -1,9 +1,11 @@
 require 'motion-require'
 
 files = [
+  "_stdlib/array",
   "concern",
   "descendants_tracker",
-  "callbacks"
+  "callbacks",
+  "core_ext/kernel/singleton_class"
 ].map { |file| File.expand_path(File.join(File.dirname(__FILE__), "/../../motion", "#{file}.rb")) }
 
 Motion::Require.all(files)
