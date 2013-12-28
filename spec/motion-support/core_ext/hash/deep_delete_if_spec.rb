7089fb9092c6a8a4b6e0d823e604fdde1eb04b97
Thomas Kadauke <thomas.kadauke@googlemail.com>
2013-12-28 15:44:13 +0100

    Adhere to naming convention in specs

--- spec/motion-support/core_ext/hash/deep_delete_if_spec.rb
+++ spec/motion-support/core_ext/hash/deep_delete_if_spec.rb
@@ -3,15 +3,17 @@ describe "hash" do
     @hash = { :a => 1, :b => [ { :c => 2, :d => 3 }, :b ], :d => { :f => 4 } }
   end
 
-  it "recursive deletes a top level key" do
-    @hash.deep_delete_if { |k,v| k == :a }.should == { :b => [ { :c => 2, :d => 3 }, :b ], :d => { :f => 4 } }
-  end
+  describe "deep_delete_if" do
+    it "should delete a top level key recursively" do
+      @hash.deep_delete_if { |k,v| k == :a }.should == { :b => [ { :c => 2, :d => 3 }, :b ], :d => { :f => 4 } }
+    end
 
-  it "recurisive deletes a key within an array" do
-    @hash.deep_delete_if { |k,v| v == 2 }.should == { :a => 1, :b => [ { :d => 3 }, :b ], :d => { :f => 4 } }
-  end
+    it "should delete a key within an array recursively" do
+      @hash.deep_delete_if { |k,v| v == 2 }.should == { :a => 1, :b => [ { :d => 3 }, :b ], :d => { :f => 4 } }
+    end
 
-  it "recursive deletes a key within a hash" do
-    @hash.deep_delete_if { |k,v| v == 4 }.should == { :a => 1, :b => [ { :c => 2, :d => 3 }, :b ], :d => {} }
+    it "should delete a key within a hash recursively" do
+      @hash.deep_delete_if { |k,v| v == 4 }.should == { :a => 1, :b => [ { :c => 2, :d => 3 }, :b ], :d => {} }
+    end
   end
 end
