2d2776e2469f53f27929c5dac2506e9cd10a5449
kayhide <kayhide@gmail.com>
2015-09-25 00:05:40 +0200

    update Object#in? and add Object#presence_in.

--- motion/core_ext/object/inclusion.rb
+++ motion/core_ext/object/inclusion.rb
@@ -6,10 +6,21 @@ class Object
   #   'Konata'.in?(characters) # => true
   #
   # This will throw an ArgumentError it doesn't respond to +#include?+.
-  def __in_workaround(args)
+  def in?(args)
     args.include?(self)
   rescue NoMethodError
     raise ArgumentError.new("The parameter passed to #in? must respond to #include?")
   end
-  alias in? __in_workaround
+
+  # Returns the receiver if it's included in the argument otherwise returns +nil+.
+  # Argument must be any object which responds to +#include?+. Usage:
+  #
+  #   params[:bucket_type].presence_in %w( project calendar )
+  #
+  # This will throw an ArgumentError if the argument doesn't respond to +#include?+.
+  #
+  # @return [Object]
+  def presence_in(another_object)
+    self.in?(another_object) ? self : nil
+  end
 end
--- spec/motion-support/core_ext/object/inclusion_spec.rb
+++ spec/motion-support/core_ext/object/inclusion_spec.rb
@@ -5,7 +5,7 @@ class LifeUniverseAndEverything
 end
 
 
-describe 'array' do
+describe 'Object' do
   describe "in?" do
     it "should support arrays" do
       1.in?([1,2,3]).should == true
@@ -29,6 +29,20 @@ describe 'array' do
     it "should support anything that implements `include?`" do
       42.in?(LifeUniverseAndEverything.new).should == true
       0.in?(LifeUniverseAndEverything.new).should == false
+    end
+  end
+
+  describe "presence_in" do
+    it "should return self when included" do
+      "stuff".presence_in(%w( lots of stuff )).should == "stuff"
+    end
+
+    it "should return nil when not included" do
+      "stuff".presence_in(%w( lots of crap )).should == nil
+    end
+
+    it "should raise ArgumentError when arg does not respond to `include?`" do
+      lambda { 1.presence_in(1) }.should.raise ArgumentError
     end
   end
 end
